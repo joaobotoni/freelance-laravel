74f44a12e727a38c4d7cb49b42f18071e008dd53
joaobotoni <joaobotoni@hotmail.com>
2024-10-12 12:13:02 -0400

    eighth commit

--- resources/views/components/project/card.blade.php
+++ resources/views/components/project/card.blade.php
@@ -2,7 +2,8 @@
     <div class="flex items-start justify-between pb-4">
         <div class="flex flex-col gap-[16px]">
             <div>
-                <span class="bg-[#C0F7B4] text-[#1D8338] rounded-full font-bold text-center uppercase py-[6px] px-[14px] text-[12px] tracking-wide ">
+                <span
+                    class="bg-[#C0F7B4] text-[#1D8338] rounded-full font-bold text-center uppercase py-[6px] px-[14px] text-[12px] tracking-wide ">
                     {{ $project->status->label() }}
                 </span>
             </div>
@@ -14,19 +15,8 @@
             </div>
         </div>
         <div>
-
-    
-        <livewire:proposals.create :$project />
-           
-            <div class="flex items-center justify-between mt-4 text-[14px]">
-                <div class="text-[#8C8C9A]  leading-6">Encerra em:</div>
-                <div class="font-bold flex items-center space-x-1">
-                    <span class="text-white ">01</span><span>:</span>
-                    <span class="text-white ">12</span><span>:</span>
-                    <span class="text-white ">26</span><span>:</span>
-                    <span class="text-white ">64</span>
-                </div>
-            </div>
+            <livewire:proposals.create :$project />
+            <livewire:projects.timer :$project />
         </div>
     </div>
 
@@ -52,7 +42,7 @@
                 </div>
                 <div class="flex items-center space-x-[4px]">
                     @foreach(range(1, $project->author->stars) as $star)
-                        <x-ui.icons.star class="h-[14px]"/>
+                        <x-ui.icons.star class="h-[14px]" />
                     @endforeach
                 </div>
             </div>
